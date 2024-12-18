8548e259b44026913bf723720b9299888c8cddb6
Saad el quaul <saadelquaul@gmail.com>
2024-12-18 23:22:53 +0100

    Implementing user registration

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,12 @@
+<?php 
+
+include '../includes/database.php';
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- pages/signup.php
+++ pages/signup.php
@@ -1,3 +1,37 @@
+<?php 
+include '../includes/database.php';
+if(isset($_POST['email'])){
+    $email = $_POST['email'];
+    $userName = $_POST['username'];
+    $password = $_POST['password'];
+    $confirmPas = $_POST['confirmPassword'];
+    
+    $sql = "SELECT * FROM users WHERE Email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0){
+        echo "<script> isExist = true;</script>";
+        return;
+    }
+    if(strlen($password) < 8 || strlen($password) > 20){
+        echo "Password must be between 8 and 20 characters long";
+        exit;
+    }
+    if($password!== $confirmPas){
+        echo "Passwords do not match";
+        exit;
+    }
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (Name, Email, Password) VALUES ('$userName', '$email', '$hashedPassword')";
+    mysqli_query($conn, $sql);
+    echo "Registration successful";
+
+
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +41,7 @@
     <title>W&R | Sign up</title>
 </head>
 <body>
-<?php include'../includes/header.php'; ?>
+<?php include '../includes/header.php'; ?>
     <main>
         <div class="container">
             <h2>Sign up</h2>
@@ -35,6 +69,7 @@
             </form>
         </div>
     </main>
-<?php include'../includes/footer.php'; ?>    
+<?php include'../includes/footer.php'; ?> 
+<script src="../js/signup.Js"></script>   
 </body>
 </html>
